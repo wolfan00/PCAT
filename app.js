f117b515e1dc20a1f4208c8bbe0af274025a4a10
wolfan00 <burhanerkulu00@gmail.com>
2025-02-28 14:53:23 +0300

    güncelleme

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
+
+
+//middle wares
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    const photo = {
-        id:1,
-        name:"photo Name",
-        description:"photo Description"
-    }
-  res.send(photo)
+   res.sendFile(path.resolve(__dirname,".temp/index.html"))
 })
 app.listen(port, () => console.log(`Sunucu ${port}! portunda başlatıldı.`))
