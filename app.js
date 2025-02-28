b97b7ee41b9ac66db9fc0435f0bffdef51389aa0
wolfan00 <burhanerkulu00@gmail.com>
2025-02-28 17:38:41 +0300

    added ejs

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = 3000
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+const app = express();
+const port = 3000;
 
+//template engine
+app.set('view engine', 'ejs');
 
 //middle wares
-app.use(express.static('public'))
-
+app.use(express.static('public'));
+//routes
 app.get('/', (req, res) => {
-   res.sendFile(path.resolve(__dirname,".temp/index.html"))
-})
-app.listen(port, () => console.log(`Sunucu ${port}! portunda başlatıldı.`))
+  //res.sendFile(path.resolve(__dirname, '.temp/index.html'));
+  res.render(`index`);
+});
+app.get('/about', (req, res) => {
+  //res.sendFile(path.resolve(__dirname, '.temp/index.html'));
+  res.render(`about`);
+});
+app.get('/contact', (req, res) => {
+  //res.sendFile(path.resolve(__dirname, '.temp/index.html'));
+  res.render(`contact`);
+});
+app.get('/add', (req, res) => {
+    //res.sendFile(path.resolve(__dirname, '.temp/index.html'));
+    res.render(`add`);
+  });
+app.listen(port, () => console.log(`Sunucu ${port}! portunda başlatıldı.`));
