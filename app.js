1255cba64318e213d49ba8a9a2708cce6d1d1ed7
wolfan00 <burhanerkulu00@gmail.com>
2025-02-28 13:42:18 +0300

    .

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-const fn = function () {
-  console.log(`function working...`);
-};
+const express = require('express')
+const app = express()
+const port = 3000
+app.get('/', (req, res) => {
+    const photo = {
+        id:1,
+        name:"photo Name",
+        description:"photo Description"
+    }
+  res.send(photo)
+})
+app.listen(port, () => console.log(`Sunucu ${port}! portunda başlatıldı.`))
