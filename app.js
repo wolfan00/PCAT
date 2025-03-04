298e5ef96db3594efc52dff7646a71c03c2de595
wolfan00 <burhanerkulu00@gmail.com>
2025-03-04 18:28:23 +0300

    method-override

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
-const { connection, createPhotos,getPhotos,getPhotosByID} = require(`./modules/photos`);
+const fileUpload = require('express-fileupload');
+const methodoverride = require('method-override');
+const { connection, createPhotos, getPhotos, getPhotosByID,updateToPhoto } = require(
+  `./modules/photos`
+);
 const app = express();
 const port = 3000;
 
@@ -9,15 +13,17 @@ const port = 3000;
 app.set('view engine', 'ejs');
 
 //middle wares
+app.use(fileUpload());
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(methodoverride('_method'));
 //routes
-app.get
+app.get;
 app.get('/', (req, res) => {
   //res.sendFile(path.resolve(__dirname, '.temp/index.html'));
-  const photos = getPhotos((err,Photos)=>{
-    res.render(`index`,{photos:Photos});
+  const photos = getPhotos((err, Photos) => {
+    res.render(`index`, { photos: Photos });
   });
 });
 app.get('/about', (req, res) => {
@@ -32,11 +38,18 @@ app.get('/add', (req, res) => {
   //res.sendFile(path.resolve(__dirname, '.temp/index.html'));
   res.render(`add`);
 });
-app.post(`/photos`, async(req, res) => {
+app.post(`/photos`, async (req, res) => {
+
+  
+const uploadImage = req.files.image
+const uploadPath = __dirname + `/public/uploads/${uploadImage.name}`
+uploadImage.mv(uploadPath, async () => {
   await createPhotos(req);
   res.redirect(`/`);
 });
-app.get("/photos/:id", (req, res) => {
+  
+});
+app.get('/photos/:id', (req, res) => {
   getPhotosByID(req.params.id, (err, Photos) => {
     if (err) {
       console.log(err);
@@ -44,6 +57,23 @@ app.get("/photos/:id", (req, res) => {
     }
     res.render('photos', { Photos });
   });
-  
+});
+app.put('/photos/:id', (req, res) => {
+  updateToPhoto(req,req.params.id, (err, Photos) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.redirect(`/photos/${req.params.id}`);
+  });
+});
+app.get('/photos/edit/:id', async (req, res) => {
+  await getPhotosByID(req.params.id, (err, Photos) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.render('edit', { Photos });
+  });
 });
 app.listen(port, () => console.log(`Sunucu ${port}! portunda başlatıldı.`));
